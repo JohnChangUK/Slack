53ee8d4d05ee1c463917fdd3b276bfd492da7f0a
John Chang <johnchang9094@gmail.com>
2017-06-16 16:15:04 +0100

    Initializing Turbo SDK

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,14 @@
 // your custom app logic goes here:
 (function(){
-
   console.log("Hello Slack!");
+
+  // Initialize Turbo SDK
+  var turbo = Turbo({ site_id: '593d9d81fd173e00115bbb26' });
+
+  $('#btn-sign-up').click(function(event){
+    event.preventDefault()
+
+    console.log("SIGN-UP!")
+  })
 
 })()
