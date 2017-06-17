e02d7ee0493b55712277d0c2ea703ce439f6e550
John Chang <johnchang9094@gmail.com>
2017-06-17 12:06:37 +0100

    Registering User when Sign Up to Slack

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,7 +8,27 @@
   $('#btn-sign-up').click(function(event){
     event.preventDefault()
 
-    console.log("SIGN-UP!")
+    var email = $('#input-sign-up').val()
+    if (email == null) {
+      alert('Please enter your email address')
+      return;
+    }
+
+    if (email.length == 0) {
+      alert('Please enter your email address')
+      return;
+    }
+
+    console.log("SIGN-UP: " + email);
+
+    turbo.createUser({email: email}, function(err, data){
+      if (err){
+        alert('Error: ' + err.message)
+        return
+      }
+      console.log('USER REGISTERED: ' + JSON.stringify(data));
+      window.location.href = '/get-started';
+    })
   })
 
 })()
